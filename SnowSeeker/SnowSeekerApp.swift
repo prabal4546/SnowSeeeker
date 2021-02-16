a0b9564899a52a8378cb3d592d29b4f33f85c8f7
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-02-16 11:07:14 +0530

    first commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by PRABALJIT WALIA     on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
